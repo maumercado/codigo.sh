dee3c11eeae081b09b18a6eb677791885489017b
Mauricio Mercado <mauricio.mercado@telus.com>
2020-03-17 23:07:06 -0700

    initial setup

---
+++ src/routes/about.svelte
@@ -0,0 +1,35 @@
+<style>
+  figure {
+    float: right;
+    margin: -3em -7em 2em 2em;
+    max-width: 400px;
+  }
+  figure img {
+    border-radius: 8px;
+  }
+  @media (max-width: 1020px) {
+    figure {
+      float: none;
+      margin: 0 auto 2em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<div class="container">
+  <p>
+    Hi, I'm <strong>Mauricio</strong>. I'm a software engineer from Barranquilla, Colombia, I currently reside in beautiful Vancouver, BC. Canada!
+    You can follow me on <a href="https://www.twitter.com/maumercado" target="_blank">Twitter</a>,
+    see some of my work on <a href="https://www.github.com/maumercado" target="_blank">GitHub</a>,
+    or visit my <a href="https://www.linkedin.com/a/mauromercado" target="_blank">Linkedin</a> profile.
+  </p>
+  <p>
+    I’ve been software engineer for almost 9+ years now, I started with Python and Python/Django,
+    then Ruby and Ruby on Rails, and lastly Javascript focusing mostly on the server side with NodeJS.
+    I love new challenges, and always striving to learn new things! Lately I have being learning Go, Typescript,
+    Terraform and CI/CD with GCP.
+  </p>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,68 @@
+<style>
+  .home-container {
+    align-items: center;
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    margin: 2em 0;
+    min-height: 400px;
+  }
+  .home-copy {
+    flex: 1;
+  }
+  h1 {
+    font-weight: 500;
+    margin-bottom: 0.5em;
+  }
+  p {
+    font-size: 1.4em;
+    line-height: 1.5;
+  }
+  figure {
+    margin: 0 1em;
+    text-align: center;
+  }
+  figcaption {
+    font-size: .8em;
+    font-style: italic;
+  }
+  img {
+    width: 100%;
+    max-width: 400px;
+    min-width: 400px;
+  }
+  @media (max-width: 1020px) {
+    p {
+      font-size: 1.2em;
+    }
+    img {
+      max-width: 400px;
+      min-width: 400px;
+    }
+  }
+  @media (max-width: 800px) {
+    .home-container {
+      flex-direction: column;
+    }
+    .home-copy {
+      flex: 0;
+      padding-bottom: 2em;
+      text-align: center;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Sapper Blog Template</title>
+</svelte:head>
+
+<div class="home-container">
+  <div class="home-copy">
+    <h1>Codigo</h1>
+  </div>
+
+  <figure>
+    <img alt='Person walking with code in background' src='undraw_code_review_l1q9.svg'>
+    <figcaption>Illustration thanks to <a href="https://undraw.co" target="_blank">Undraw</a></figcaption>
+  </figure>
+</div>
